8630facbfbcd6290adfc54a8c690966e09eb0e70
Steve <steven.byrnes@gmail.com>
2014-01-02 09:59:14 -0500

    add Python 3 as a classifier
    I haven't tested it in Python 3, but it probably works, and it's
    "supported" in the sense that I will fix any Python-3-related bug that
    anyone tells me. I have already added this classifier to PyPI directly.

--- setup.py
+++ setup.py
@@ -30,5 +30,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         ]
 )
